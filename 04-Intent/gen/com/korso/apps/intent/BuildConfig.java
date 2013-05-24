1a04773285a386a98d2deb5d339cdbb78a378a58
dimitri001 <dimitri001+github@gmail.com>
2013-05-24 18:09:27 +0200

    ListActivity

---
+++ 04-Intent/gen/com/korso/apps/intent/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.korso.apps.intent;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ListActivity/src/org/korso/apps/listactivity/MyListActivity.java
@@ -0,0 +1,32 @@
+package org.korso.apps.listactivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MyListActivity extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		String []datos = getResources().getStringArray(R.array.datos);
+		
+		ArrayAdapter <String> adapter = new ArrayAdapter <String>(this, android.R.layout.simple_list_item_1, datos);
+		setListAdapter(adapter);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		Toast.makeText(this, 
+				(String) l.getItemAtPosition(position), 
+				Toast.LENGTH_LONG).show();	
+	}
+
+	
+}
